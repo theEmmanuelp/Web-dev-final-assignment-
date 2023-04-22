d39ca314940ae8a596547b8831152f4584162a7c
theEmmanuelp <emmanuel_pagaduan@outlook.com>
2023-04-22 13:56:23 -0600

    this is first commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 //All your code goes here
 
+app.get("/courses", (req, res) => {
+    let Course = JSON.parse(fs.readFileSync("database/courses.json"));
+    res.json(Course); 
+})
+
+app.get("/account/:id", (req,res) => {
+    console.log(req.params.id);
+})
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = app;
