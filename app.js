a21a3caae8325c6b430b5d783551b3dcb7be8195
theEmmanuelp <emmanuel_pagaduan@outlook.com>
2023-04-26 12:28:20 -0600

    i have no idea why nothing is working

--- app.js
+++ app.js
@@ -6,19 +6,65 @@ const app = express();
 // serving static files
 app.use(express.static("public"));
 
-
-
 //Get/courses
     //should respond
 app.get("/courses", (req, res) => {
     let Course = JSON.parse(fs.readFileSync("database/courses.json"));
     res.json(Course); 
+
 })
 
 app.get("/account/:id", (req,res) => {
     console.log(req.params.id);
 })
-    //should accept query parameters
+
+//should accept query parameters
+app.get("/courses", (req, res) => {
+    let courses = JSON.parse(fs.readFileSync("database/courses.json"));
+    let { code, num } = req.query;
+    if (code) {
+      courses = courses.filter((course) => course.code === code);
+    }
+    if (num) {
+      if (num.length === 4) {
+        courses = courses.filter((course) => course.num === num);
+      } else if (num.length === 1) {
+        courses = courses.filter((course) => course.num[0] === num);
+      }
+    }
+    res.json(courses);
+  });
+   
+// Get/account/:d
+app.get("/account/:id", (req,res) => {
+    const users = JSON.parse(fs.readFileSync("database/users.json"));
+    const id = req.params.id;
+    const user = users.find((user) => user.id === id);
+
+    if (user) {
+        // Remove the password property from the user object
+        const { password, ...userWithoutPassword } = user;
+        res.json({ user: userWithoutPassword });
+    } else {
+        res.status(404).json({ error: "User not found" });
+    }
+});
+
+// Post /users/signup
+app.post("/users/signup", (req, res) => {
+    const users = JSON.parse(fs.readFileSync("database/users.json"));
+    const { username, password } = req.body;
+    const newUser = {
+        username,
+        password,
+        id: uuidv4(),
+        courses: [],
+    };
+    users.push(newUser);
+    fs.writeFileSync("database/users.json", JSON.stringify(users, null, 2));
+    res.status(201).json({ message: "User created", id: newUser.id });
+});
+
 
   
     
